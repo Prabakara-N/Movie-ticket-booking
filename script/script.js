4ea0397c2e3401de66c11fb8c5df6156ce910799
prabakaran <mspk028@gmail.com>
2023-02-06 22:01:40 +0530

    local storage added

--- script/script.js
+++ script/script.js
@@ -2,11 +2,12 @@
 
 // elements
 const selectEl = document.getElementById("movie-options");
-// const seatsEl=document.querySelectorAll('.')
 const screenContainer = document.querySelector(".seat-container");
 const countEl = document.getElementById("count");
 const totalEl = document.getElementById("total");
+const unSelectedSeatsEl = document.querySelectorAll(".seats .seat:not(.sold)");
 
+populateUI();
 // global variables
 let ticketPrize = Number(selectEl.value);
 let seatCount = 0;
@@ -28,18 +29,55 @@ function showImage() {
   }
 }
 
-function populateUI() {
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrize) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrize", moviePrize);
+}
+
+// updating count & ticket price respectively
+function updatePrize() {
+  const selectedSeatsEl = document.querySelectorAll(".seats .seat.selected");
+  seatCount = selectedSeatsEl.length;
+  // displaying count & prize when change
   countEl.innerText = seatCount;
   totalEl.innerText = seatCount * ticketPrize;
+
+  // adding local storage
+  const seatIndex = [...selectedSeatsEl].map((seat) => {
+    return [...unSelectedSeatsEl].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
+}
+
+// getting data from local storage & display to the UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    unSelectedSeatsEl.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    selectEl.selectedIndex = selectedMovieIndex;
+  }
 }
 
 // event listneres
 // selecting movie
-selectEl.addEventListener("change", () => {
+selectEl.addEventListener("change", (e) => {
   // calling show image function
   showImage();
   ticketPrize = Number(selectEl.value);
-  populateUI();
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updatePrize();
 });
 
 // selecting seats
@@ -47,11 +85,11 @@ screenContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("seat")) {
     if (!e.target.classList.contains("sold")) {
       e.target.classList.toggle("selected");
-      const selectedSeatsEl = document.querySelectorAll(
-        ".seats .seat.selected"
-      );
-      seatCount = selectedSeatsEl.length;
-      populateUI();
+      // calling update count & prize function
+      updatePrize();
     }
   }
 });
+
+// whenever page loads count & prize will show
+updatePrize();
